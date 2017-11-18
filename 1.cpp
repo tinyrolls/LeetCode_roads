9a36049300d3708f4755a83bdecf5fd8aee2cf87
Tinyrolls <tinyrolls@Summer-War.local>
2017-11-18 22:09:35 +0800

    1. Two Sum

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hash;
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++) {
+            int NumbertoFind = target - nums[i];
+            if (hash.find(NumbertoFind) != hash.end()) {
+                result.push_back(hash[NumbertoFind]);
+                result.push_back(i);
+                return result;
+            }
+            hash[nums[i]] = i;
+        }
+        return result;
+    }
+};
